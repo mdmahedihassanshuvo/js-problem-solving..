d61b74d042d05537d1efb8c5db7155c0fea56173
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-01-31 19:54:21 +0600

    javascript practice

---
+++ 2d-array.js
@@ -0,0 +1,36 @@
+// for(var i=0;i<5;i++){
+//   setTimeout(()=>console.log(i), 10000);// 5 5 5 5 5
+// }
+
+// function multiplication(num){
+//   let gunon = [];
+//   for(var i = 1 ; i <= num ; i++){
+//     // console.log(i);
+//     let element = i * 13;
+//     gunon.push(element);
+//   }
+//   return gunon;
+// }
+
+// var gunon = multiplication(10);
+// console.log(gunon);
+
+
+// function multiplicaiton_table(n){
+//   let table = [];
+//   for (let i = 1; i <= n; i++) {
+//   let result = i * 13;
+//   table.push(result)
+//   }
+//   return table;
+//   }
+//   console.log(multiplicaiton_table(10));
+
+function dharapatThirteen(num){
+  var i=1;
+  while(i<=10){
+  console.log(num*i);
+  i++;
+  }
+  } var number=13;
+  dharapatThirteen(number);
